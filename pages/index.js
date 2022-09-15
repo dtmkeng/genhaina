44d31dc8904a131d2eee2105ecc93d1c200615ea
dtmkeng <phumaisut@gmail.com>
2022-09-15 22:32:24 +0700

    init genhaina

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='flex w-full justify-center p-16 flex-col items-center'>
+      <h1 className='text-3xl'>Random citizen number</h1>
+      <span className="text-sm text-red-600">It is for system testing purposes only.</span>
+      <div></div>
+    </div>
+  );
+}
